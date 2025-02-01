7612ce896dda322eda29ba7c19f66e8a77a52f87
Tolfx <matteus@tolfx.dev>
2025-02-01 17:53:24 +0100

    fix: comment

--- internal/templates/udl_server.go
+++ internal/templates/udl_server.go
@@ -174,7 +174,7 @@ spec:
             - name: SRCDS_STARTMAP
               value: {{ or .Map "tfdb_octagon_odb_a1" }}
             - name: SRCDS_STATIC_HOSTNAME
-              value: UDL.TF | {{ .Division }} | Match #{{ .MatchID }}
+              value: "UDL.TF | {{ .Division }} | Match #{{ .MatchID }}"
             - name: SRCDS_TOKEN
               value: {{ .SRCDSToken }}
             - name: MATCH_ID
