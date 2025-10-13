0ea0510f331ee01aec4de32f4887606cf86731ee
Tolfx <matteus@tolfx.dev>
2025-10-13 21:51:27 +0200

    fix: correct indentation for nodePort configuration in udl_server template

--- internal/templates/udl_server.go
+++ internal/templates/udl_server.go
@@ -268,26 +268,26 @@ spec:
       protocol: UDP
       port: {{ .Port }}
       targetPort: {{ .Port }}
-			nodePort: {{ .Port }}
+      nodePort: {{ .Port }}
     - name: game-tcp
       protocol: TCP
       port: {{ .Port }}
       targetPort: {{ .Port }}
-			nodePort: {{ .Port }}
+      nodePort: {{ .Port }}
     - name: sourcetv
       protocol: UDP
       port: {{ .SourceTVPort }}
       targetPort: {{ .SourceTVPort }}
-			nodePort: {{ .SourceTVPort }}
+      nodePort: {{ .SourceTVPort }}
     - name: clientport
       protocol: UDP
       port: {{ .ClientPort }}
       targetPort: {{ .ClientPort }}
-			nodePort: {{ .ClientPort }}
+      nodePort: {{ .ClientPort }}
     - name: steamport
       protocol: UDP
       port: {{ .SteamPort }}
       targetPort: {{ .SteamPort }}
-			nodePort: {{ .SteamPort }}
+      nodePort: {{ .SteamPort }}
   type: NodePort
 `
